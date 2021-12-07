23ce0e8534a7ac33de190bf46304824afdbd3494
joshuavottero <joshuavottero@outlook.com>
2021-12-07 18:43:45 +0000

    addded nodmon

--- express_server.js
+++ express_server.js
@@ -12,12 +12,12 @@ const generateRandomString = function() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = ' ';
   const charactersLength = characters.length;
-  for ( let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
  
   return result;
-}
+};
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
