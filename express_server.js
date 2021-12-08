a84879501717909be8c963a28f3c4c380d4d5fbe
joshuavottero <joshuavottero@outlook.com>
2021-12-08 01:37:49 +0000

    added login and logout routes with updated functional in partials _header

--- express_server.js
+++ express_server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 // function below used logic from https://www.programiz.com/javascript/examples/generate-random-strings  (example 1)
 const generateRandomString = function() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -28,18 +30,35 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.userName);
+  console.log('Cookies: ', req.cookies)
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b><body></html>\n');
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"], 
+    urls: urlDatabase
+   };
   res.render('urls_index', templateVars);
 });
 
 
 app.get('/urls/new', (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -52,7 +71,7 @@ app.post("/urls", (req, res) => {
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  res.redirect(`/urls`);
+  res.redirect("/urls");
  
 });
 
@@ -76,7 +95,11 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; // longURL may need to change
+  const templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  }; // longURL may need to change
   res.render('urls_show', templateVars);
 });
 
